e7d98a15d3121b1f3450b4a0a737b931c1233103
Ronney Ramon <ronney@ymail.com>
2020-06-05 11:58:58 -0300

    Mobile app.

---
+++ mobile/App.tsx
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import { AppLoading } from 'expo';
+
+import Routes from './src/routes';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent"></StatusBar>
+      <Routes />
+    </Fragment>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#826987',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
